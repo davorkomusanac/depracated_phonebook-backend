f5a91289ba915ee957021afb3bc33a09d6a8d88b
Davor <davor.komusanac@gmail.com>
2022-01-29 19:18:35 +0100

    uibuild

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
+const Contact = require("./models/contact");
+
+//Load middleware
+app.use(express.static("build"));
 app.use(express.json());
 app.use(
   morgan(
@@ -9,95 +14,122 @@ app.use(
   )
 );
 app.use(cors());
-app.use(express.static("build"));
 
 morgan.token("postData", (request, response) => {
   return JSON.stringify(request.body);
 });
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-app.post("/api/persons", (request, response) => {
+// CREATE CONTACT
+app.post("/api/contacts", (request, response, next) => {
   const body = request.body;
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "Both fields 'name' and 'number' are required",
-    });
-  }
+  Contact.find({ name: body.name }).then((result) => {
+    if (result.length !== 0) {
+      return response.status(400).json({
+        error: "name must be unique",
+      });
+    } else {
+      const contact = new Contact({
+        name: body.name,
+        number: body.number,
+      });
+      contact
+        .save()
+        .then((savedContact) => response.json(savedContact))
+        .catch((error) => next(error));
+    }
+  });
+});
 
-  if (persons.find((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
+// GET CONTACTS
+app.get("/api/contacts", (request, response) => {
+  Contact.find({}).then((contacts) => response.json(contacts));
+});
 
-  const person = {
-    id: Math.round(Math.random() * 1000),
+// GET INFO
+app.get("/info", (request, response) => {
+  Contact.find({})
+    .then((contacts) => {
+      const info = {
+        length: contacts.length,
+        date: new Date(),
+      };
+
+      response.send(
+        `<div><p>Phonebook has info for ${info.length} people</p><p>${info.date}</p></div>`
+      );
+    })
+    .catch((error) => next(error));
+});
+
+// GET SPECIFIC CONTACT
+app.get("/api/contacts/:id", (request, response, next) => {
+  Contact.findById(request.params.id)
+    .then((contact) => {
+      if (contact) {
+        response.json(contact);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+//UPDATE CONTACT
+app.put("/api/contacts/:id", (request, response, next) => {
+  const body = request.body;
+  const contact = {
     name: body.name,
     number: body.number,
   };
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  Contact.findByIdAndUpdate(request.params.id, contact, {
+    new: true,
+    runValidators: true,
+  })
+    .then((updatedContact) => {
+      if (updatedContact) {
+        response.json(updatedContact);
+      } else {
+        next(updatedContact);
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
+// DELETE CONTACT
+app.delete("/api/contacts/:id", (request, response, next) => {
+  Contact.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      console.log(result);
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/info", (request, response) => {
-  const info = {
-    length: persons.length,
-    date: new Date(),
-  };
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-  response.send(
-    `<div><p>Phonebook has info for ${info.length} people</p><p>${info.date}</p></div>`
-  );
-});
+app.use(unknownEndpoint);
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
+//EROR HANDLER
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
 
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).send({ error: error.message });
   }
-});
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+  next(error);
+};
 
-  response.status(204).end();
-});
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001;
+//LISTEN TO APP
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ models/contact.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const url = process.env.MONGODB_URI;
+
+console.log(`connecting to ${url}`);
+
+mongoose
+  .connect(url)
+  .then((result) => console.log(`connected to MongoDB - ${result}`))
+  .catch((error) => console.log(`error connecting to MongoDB - ${error}`));
+
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+    validate: {
+      validator: function (v) {
+        const a = /\d{2}-\d{6,}/.test(v);
+        const b = /\d{3}-\d{5,}/.test(v);
+        const c = /^[0-9]{8,}$/.test(v);
+        return a || b || c;
+      },
+      message: "Wrong number format",
+    },
+  },
+});
+
+contactSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Contact", contactSchema);
